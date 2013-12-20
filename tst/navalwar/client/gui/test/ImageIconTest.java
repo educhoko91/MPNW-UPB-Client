58567016b8914cab37e0f15433fe9dacda123c71
educhoko91 <educhoko91@gmail.com>
2013-12-20 11:07:35 -0400

    Agregado Test Unitario

---
+++ tst/navalwar/client/gui/test/ImageIconTest.java
@@ -0,0 +1,33 @@
+package navalwar.client.gui.test;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+
+import javax.swing.ImageIcon;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImageIconTest {
+	
+	ImageIcon icon;
+	FileNotFoundException e;
+
+	@Test(expected= FileNotFoundException.class)
+	public void testWrongFile() {
+		icon = new ImageIcon("None Existing File");
+	}
+	
+	@Test
+	public void testCorretFile() {
+		icon = new ImageIcon("res/empty_cell.jpg");
+	}
+	
+	@Test
+	public void testExpectedNull() {
+		icon = new ImageIcon("None Existing File");
+		assertNull(icon);
+	}
+
+}
